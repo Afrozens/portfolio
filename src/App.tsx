dd8d74de6c862575e3b01a213a3c0aabe29ab7e4
Jesus Chacon <jesuszen2.6@gmail.com>
2026-06-28 02:22:44 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Toaster } from "react-hot-toast";
 import { Header } from "./components";
 import { HomePage, AboutPage, ProjectPage, ContactPage } from "./pages";
-import ButtonUp from "./components/PrimitiveElements/ButtonUp";
 
 const App = () => {
   return (
